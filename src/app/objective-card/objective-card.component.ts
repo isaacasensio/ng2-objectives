5e5534e1f4bb8c910550a249ab6afc10c5e1a390
Jonathan Sharpe <jsharpe@pivotal.io>
2016-11-16 18:24:15 +0000

    Show people to notify
    Signed-off-by: Isaac Asensio <iasensiomejia@pivotal.io>

--- src/app/objective-card/objective-card.component.ts
+++ src/app/objective-card/objective-card.component.ts
@@ -20,6 +20,7 @@ export class ObjectiveCardComponent implements OnInit {
     date: '2016/09/12',
     completedOn: null,
     notes: 'Description of this thing',
+    notify: ['Hans Muster', 'Jane Doe'],
   };
 
   get targetSummary(): string {
@@ -42,6 +43,13 @@ export class ObjectiveCardComponent implements OnInit {
       .debounceTime(300)
       .map(values => Object.assign(this.objective, values))
       .subscribe(console.log);
+  }
+
+  deletePerson(name: string) {
+    let index = this.objective.notify.indexOf(name, 0);
+    if (index > -1) {
+      this.objective.notify.splice(index, 1);
+    }
   }
 
 }
